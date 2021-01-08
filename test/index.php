74deaf14e19863ebb2e9d3c6f9aa1f98f5bb51eb
Lucas <lhcamargo3@gmail.com>
2021-01-08 08:38:48 -0300

    Test fix

--- test/index.php
+++ test/index.php
@@ -5,9 +5,8 @@ require __DIR__ . '/../lib_ext/autoload.php';
 
 use Notification\Email;
 
-$novoEmail = new Email(2, 'smtp.gmail.com', "lhcamargo3@gmail.com", "Goodam123", "ssl", 465, "lhcamargo3@gmail.com",
-    "Lucas Camargo");
-$novoEmail->sendMail("Assunto de Teste", "<p>Esse é um e-mail de <b>teste</b></p>", "lhcamargo3@gmail.com", "Lucas",
-    "lhcamargo3@gmail.com", "Camargo");
+$email = new Email(2, "mail.host.com", "your@email.com", "your-pass", "smtp secure (tls / ssl)", "port (587)", "from@email.com", "From name");
+
+$email->sendEmail("SUbject", "Content", "reply@email.com", "Replay Name", "address@email.com", "Address Name");
 
 var_dump($novoEmail);
